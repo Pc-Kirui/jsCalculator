f36afd045be8201ce2ae03921cdcc252e8c69369
Pc-Kirui <patokirui@gmail.com>
2024-05-21 22:24:30 +0300

    javascript file for making the app interactive

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// Variables for each of the calculator operation
+let firstNum;
+let secondNum;
+let operator;
+
+function add() {}
+
+function subtract() {}
+
+function multiply() {}
+
+function divide() {}
+
+// Function operate - call functions for the basic math operations
+function operate() {}
