c4b4f7e79c98279c94f44ce7644d633c2ea22e45
Pc-Kirui <patokirui@gmail.com>
2024-05-29 16:07:37 +0300

    added some calculator functionalities

--- app.js
+++ app.js
@@ -1,194 +1,129 @@
 "use strict";
 
-// Variables for each of the calculator operation
 let firstNum = "";
 let secondNum = "";
 let operator = "";
-// let equalOp = "";
-// const operators = "+-*%/";
+const allOperators = "+-*/%";
+let isEval = false; //Handling result when "=" is pressed
 
-// Getting elements using value/parameters from HTML
+//Getting elements from HTML using value/parameters
 //Display elements
 const currentScreen = document.querySelector(".current");
 const previousScreen = document.querySelector(".previous");
 
-// Numbers and operations("+","-","*","/","%")
-const numbers = document.querySelectorAll(".numbers");
-const operations = document.querySelectorAll(".operations");
-const equal = document.querySelector(".equal");
-const decimalPoint = document.querySelector(".dec-point");
-// console.log(equal);
+//Getting calculator keys
+const calcNumbers = document.querySelectorAll(".numbers");
+const calcOperators = document.querySelectorAll(".operations");
 
-//Printing the numbers on the console
-numbers.forEach((number) =>
+//Displaying calc-keys on calc-screen/console
+calcNumbers.forEach((number) =>
   number.addEventListener("click", function (e) {
-    displayNumber(e.target.textContent);
-    currentScreen.textContent = firstNum;
+    const keyNum = e.target.textContent;
+    displayNum(keyNum);
   })
 );
 
-//Printing the operator on the console
-operations.forEach((op) => {
+calcOperators.forEach((op) =>
   op.addEventListener("click", function (e) {
-    const operation = e.target.textContent;
-    handleOperand(operation);
-    if (operation === "AC") {
+    let keyOp = e.target.textContent;
+    if (
+      keyOp === "+" ||
+      keyOp === "-" ||
+      keyOp === "*" ||
+      keyOp === "/" ||
+      keyOp === "%"
+    ) {
+      displayOperator(keyOp);
+    } else if (keyOp === "=") {
+      if (!firstNum || !secondNum) return;
+      currentScreen.textContent = operate(firstNum, secondNum, operator);
+      isEval = true;
+      previousScreen.textContent = "";
+      secondNum = operator = "";
+      firstNum = currentScreen.textContent;
+    } else if (keyOp === "AC") {
       firstNum = "";
       secondNum = "";
-      currentScreen.textContent = secondNum;
-      previousScreen.textContent = firstNum;
-    } else if (operation === "DEL") {
+      currentScreen.textContent = "";
+      previousScreen.textContent = "";
+      operator = "";
+    } else {
       currentScreen.textContent = currentScreen.textContent
         .toString()
         .slice(0, -1);
-    } else if (operation === "=") {
-      //Handling the equal operator
-      op.addEventListener("click", function (eq) {
-        if (firstNum && operator && secondNum) {
-          let ans = operate(firstNum, secondNum, operator);
-          ans = roundNum(ans);
-          previousScreen.textContent = "";
-          currentScreen.textContent = ans;
-        }
-      });
-    } else {
-      previousScreen.textContent = secondNum + "" + operator;
-      currentScreen.textContent = firstNum;
     }
-  });
-});
+  })
+);
 
-//Handling the decimal point
-decimalPoint.addEventListener("click", function (e) {
-  addDecimal(e.target.textContent);
-});
-
-function displayNumber(num) {
-  if (firstNum.length <= 9) {
-    firstNum += num;
+function displayNum(num) {
+  if (operator === "") {
+    if (!isEval) {
+      if (firstNum.includes(".") && num === ".") return;
+      firstNum += num;
+      currentScreen.textContent = firstNum;
+    } else {
+      firstNum = num;
+      currentScreen.textContent = firstNum;
+      isEval = false;
+    }
+  } else {
+    if (secondNum.includes(".") && num === ".") return;
+    secondNum += num;
+    currentScreen.textContent = secondNum;
   }
 }
 
-function handleOperand(operand) {
-  operator = operand;
-  secondNum = firstNum;
-  firstNum = "";
-}
-
-function addDecimal(dec) {
-  if (!firstNum.includes(dec)) {
-    firstNum += dec;
-    currentScreen.textContent = firstNum;
+function displayOperator(op) {
+  const lastEntry = currentScreen.textContent.slice(-1);
+  if (operator) {
+    if (allOperators.includes(lastEntry)) {
+      return;
+    } else {
+      firstNum = operate(firstNum, secondNum, operator);
+      operator = op;
+      secondNum = "";
+      previousScreen.textContent = firstNum + operator;
+      currentScreen.textContent = "";
+    }
+  } else if (!operator) {
+    operator = op;
+    previousScreen.textContent = firstNum + operator;
+    currentScreen.textContent = "";
   }
 }
 
-function roundNum(num) {
-  return Math.round(num * 1000000) / 1000000;
+function addDecimal(dot) {
+  if (!firstNum.includes(dot)) {
+    firstNum += dot;
+  }
 }
 
-//Displaying operations
-
-// operations.forEach((operation) =>
-//   operation.addEventListener("click", function (e) {
-//     const op = e.target.textContent;
-//     if (op === "AC") {
-//       currentScreen.textContent = "";
-//       operator = firstNum = secondNum = equalOp = "";
-//     } else if (op === "DEL") {
-//       currentScreen.textContent = currentScreen.textContent.substring(
-//         0,
-//         currentScreen.textContent.length - 1
-//       );
-//     } else {
-//       displayOperation(op);
-//       operator += op;
-//     }
-//   })
-// );
-
-// equal.addEventListener("click", function (e) {
-//   displayEqual(e.target.textContent);
-// });
-
-// function displayNumber(num) {
-//   if (operator.length === 0) {
-//     if (firstNum.includes(".") && num === ".") return;
-//     firstNum += num;
-//     currentScreen.textContent += num;
-//   } else {
-//     if (secondNum.includes(".") && num === ".") return;
-//     secondNum += num;
-//     currentScreen.textContent += num;
-//   }
-// }
-
-// function displayOperation(op) {
-//   if (operator.length === 0) {
-//     currentScreen.textContent += ` ${op} `;
-//   } else {
-//     if (!operators.includes(currentScreen.textContent[-1])) {
-//       const ans = operate(
-//         Number(firstNum),
-//         Number(secondNum),
-//         operator.slice(-1)
-//       );
-//       currentScreen.textContent = ans;
-//       firstNum = ans;
-//       secondNum = "";
-//       currentScreen.textContent += ` ${op} `;
-
-//       //   operator = operator[-1];
-//     }
-//   }
-// }
-
-// function displayEqual(eq) {
-//   if (equalOp.length === 0) {
-//     if (secondNum === "") {
-//       currentScreen.textContent = firstNum;
-//       equalOp = "";
-//       return;
-//     }
-//     if (firstNum !== "") {
-//       equalOp += eq;
-//       currentScreen.textContent += equalOp;
-
-//       const ans = operate(
-//         Number(firstNum),
-//         Number(secondNum),
-//         operator.slice(-1)
-//       );
-//       currentScreen.textContent = ans;
-//       equalOp = "";
-//     }
-//   }
-// }
-
-//Calculator functions for the basic math operations
+//Basic math operations functions
 function add(firstNum, secondNum) {
-  return (secondNum += firstNum);
+  return firstNum + secondNum;
 }
 
 function subtract(firstNum, secondNum) {
-  return (secondNum -= firstNum);
+  return firstNum - secondNum;
 }
 
 function multiply(firstNum, secondNum) {
-  return (secondNum *= firstNum);
+  return firstNum * secondNum;
 }
 
 function modulo(firstNum, secondNum) {
-  return (secondNum %= firstNum);
+  return firstNum % secondNum;
 }
 
 function divide(firstNum, secondNum) {
-  if (firstNum === 0) {
-    return "Invalid Operation!";
+  if (secondNum === 0) {
+    return "Infinity!";
+  } else {
+    return firstNum / secondNum;
   }
-  return (secondNum /= firstNum);
 }
 
-// Function operate - call functions for the basic math operations
+//Operate function
 function operate(firstNum, secondNum, operator) {
   firstNum = parseFloat(firstNum);
   secondNum = parseFloat(secondNum);
@@ -203,13 +138,13 @@ function operate(firstNum, secondNum, operator) {
     case "*":
       return multiply(firstNum, secondNum);
 
-    case "/":
-      return divide(firstNum, secondNum);
-
     case "%":
       return modulo(firstNum, secondNum);
 
+    case "/":
+      return divide(firstNum, secondNum);
+
     default:
-      return "Enter valid numbers";
+      return "Invalid Operation!";
   }
 }
